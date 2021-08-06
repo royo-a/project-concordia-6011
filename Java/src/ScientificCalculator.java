be77367f54427d9559c90ac60f9f207fc12383e9
Arnab Roy <royo.arnab@gmail.com>
2021-08-06 00:25:20 -0400

    Refactored for tangent function

--- Java/src/ScientificCalculator.java
+++ Java/src/ScientificCalculator.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class ScientificCalculator {
 	private static long[] factorials = generateFactorials(19);
 
+	// Can generate maximum 19! without overflow
 	private static long[] generateFactorials(int maxTerm) {
 		long[] factorials = new long[maxTerm + 1];
 		factorials[0] = 1;
@@ -19,64 +20,64 @@ public class ScientificCalculator {
 		// Get the part before the decimal.
 		int wholePart = (int) number;
 		// We only want to work with the digits after the decimal.
-		// So we remove anything before the decimal.
+		// So we zero digits before the decimal.
 		number = number - wholePart;
 		// We cannot work directly on the fractional part.
 		// We need to get them as integers to do math on them.
-		// Get upto 10 digits of fractional part as integers so we can process them.
-		long numberWholePart = (long) (number * 1000000000.0);
-		// Get the last digit and check whether it is >= 5
-		// If it is, we need to add 1 to the last digit to round the number
-		int remainder = (int) (numberWholePart % 10);
-		numberWholePart /= 10;
+		// Get upto 9 digits of fractional part as integers so we can process them.
+		long fractionAsInteger = (long) (number * 1000000000.0);
+		// Get the 9th digit and check whether it is >= 5
+		// If it is, we need to add 1 to the 8th digit to round the number
+		int ninthDigit = (int) (fractionAsInteger % 10);
+		fractionAsInteger /= 10;
 
-		if (remainder >= 5) {
-			numberWholePart += 1;
+		if (ninthDigit >= 5) {
+			fractionAsInteger += 1;
 		}
 
-		// Now that our calculation is complete, we need to turn the number back to fraction
-		// We accomodate the whole part in the number first
+		// Now that our calculation is complete, we need to convert the fraction from integer -> fraction
+		// We accommodate the whole part in the number first
+		// We are leaving 8 zeroes after the whole part so that we can add the fractional part without affecting the ⤵️
+		// whole part.
 		number = wholePart * 100000000;
-		// We add the fractional digits to the number as integers
-		number += numberWholePart;
-		// We divide with 10e9 to get the fractional parts
+		// We add the fractional digits to the number in integer form
+		number += fractionAsInteger;
+		// We divide with 10e8 to get the fractional parts
 		number/=100000000;
-//        number = (double) numberWholePart / 1000000000;
-//        number += wholePart;
 
 		return number;
 	}
 
 	private static double calculateCos(int angle, long[] factorials) {
-		// Angles greater than 360 can be represented within 360 in the quadrants.
-		// We first convert the angles within 360 degrees.
+		// Angles greater than 360 can be represented within 360. This makes the calculation faster.
 		angle = angle % 360;
 
-		// For both positive and negative cos angle, the results are the same.
-		// We want to work with positive angles and reflect the sign later based on their quadrant.
+		// For both positive and negative angle, the results of cosine are the same.
+		// We want to work with positive angles for easiness and reflect the sign later based on their quadrant.
 		if (angle < 0) {
-			// make the angle positive for calculation
+			// taking absolute value of angle
 			angle *= -1;
 		}
 
-		// We'll convert the angles within 90 degree to improve calculation accuracy.
-		// We also have to keep track of the quadrants to determine the sign.
+		// We'll convert the angles within 90 degree to further improve calculation accuracy and speed.
+		// We also have to keep track of the quadrants to manually determine the sign.
 		double quadrant = (double) angle/90;
-		double quadrantSign = 1;
+		double signInQuadrant = 1;
 		if(quadrant > 1 && quadrant <= 2){
 			angle = 180-angle;
-			quadrantSign = -1;
+			signInQuadrant = -1;
 		} else if(quadrant > 2 && quadrant <= 3){
 			angle = angle-180;
-			quadrantSign = -1;
+			signInQuadrant = -1;
 		} else if(quadrant > 3){
 			angle = 360-angle;
 		}
 
+		// Calculation for Taylor series
 		boolean shouldSubtract = true;
 		double angleInRadian = angle * 3.141592654 / 180;
-		double angleInRadianSquare = angleInRadian * angleInRadian;
-		double numerator = angleInRadianSquare;
+		double squareOfRadianAngle = angleInRadian * angleInRadian;
+		double numerator = squareOfRadianAngle;
 		int digitToFactorial = 2;
 		double result = 1;
 		for (int i = 1; i <= 9; i++) {
@@ -88,46 +89,48 @@ public class ScientificCalculator {
 				shouldSubtract = true;
 			}
 			result += nTerm;
-			numerator = numerator * angleInRadianSquare;
+			numerator = numerator * squareOfRadianAngle;
 			digitToFactorial += 2;
 		}
 
 		result = roundTo8Digits(result);
-		return result == 0 ? result : result * quadrantSign;
+		// For 0, don't return any sign
+		return result == 0 ? result : result * signInQuadrant;
 	}
 
 	private static double calculateSin(int angle, long[] factorials) {
 		// Angles greater than 360 can be represented within 360 in the quadrants.
-		// We first convert the angles within 360 degrees.
 		angle = angle % 360;
 
-		// For the same angle, the results are the same, only their sign changes.
-		// We want to work with positive angles and reflect the sign later in the result.
+		// For both positive and negative angle, the results of cosine are the same, only their sign changes.
+		// We want to work with positive angles for easiness and reflect the sign later based on their quadrant and
+		// initial sign of input angle.
 		double sign = 1;
 		if (angle < 0) {
 			// remember the sign
 			sign = -1;
-			// make the angle positive for calculation
+			// taking absolute value of angle
 			angle *= -1;
 		}
 
-		// We'll convert the angles within 90 degree to improve calculation accuracy.
-		// We also have to keep track of the quadrants to determine the sign.
+		// We'll convert the angles within 90 degree to further improve calculation accuracy and speed.
+		// We also have to keep track of the quadrants to manually determine the sign.
 		double quadrant = (double) angle/90;
-		double quadrantSign = 1;
+		double signInQuadrant = 1;
 		if(quadrant > 1 && quadrant <= 2){
 			angle = 180-angle;
 		} else if(quadrant > 2 && quadrant <= 3){
 			angle = angle-180;
-			quadrantSign = -1;
+			signInQuadrant = -1;
 		} else if(quadrant > 3){
 			angle = 360-angle;
-			quadrantSign = -1;
+			signInQuadrant = -1;
 		}
 
+		// Calculation for Taylor series
 		boolean shouldSubtract = true;
 		double angleInRadian = angle * 3.141592654 / 180;
-		double angleInRadianSquare = angleInRadian * angleInRadian;
+		double squareOfRadianAngle = angleInRadian * angleInRadian;
 		double numerator = angleInRadian * angleInRadian * angleInRadian;
 		int digitToFactorial = 3;
 		double result = angleInRadian;
@@ -140,21 +143,21 @@ public class ScientificCalculator {
 				shouldSubtract = true;
 			}
 			result += nTerm;
-			numerator = numerator * angleInRadianSquare;
+			numerator = numerator * squareOfRadianAngle;
 			digitToFactorial += 2;
 		}
 
 		result = roundTo8Digits(result);
-		return result == 0 ? result : result * sign * quadrantSign;
+		return result == 0 ? result : result * sign * signInQuadrant;
 	}
 
-	public static double calculateTangent(int angle, long[] factorials) {
+	public static double calculateTan(int angle, long[] factorials) {
 		return roundTo8Digits(calculateSin(angle, factorials) / calculateCos(angle, factorials));
 	}
 	
-	public static void function3or4() {		
-		FunctionF4.gamma();
-	}
+	public static void function3or4() {
+			
+		}
 	
 	public static float power(float b, float x) {
 	    if (x < 0) {
@@ -229,8 +232,8 @@ public class ScientificCalculator {
 	public static void menu() {
 		int choice = 0;
 		Scanner input = new Scanner(System.in);
-		System.out.println("Available Function");
-		System.out.println("1. Function : tan(x) \n2. Function : Γ(x) \n3. Function : ab^x\n4. Function : x^y\n5. End");
+		System.out.println("Avaiable Function");
+		System.out.println("1. Function : tan(x) \n2. Function : sinh(x)\n3. Function : ab^x\n4. Function : x^y\n5. End");
 		System.out.print("\nPlease enter your choice (enter function number) = ");
 		try {
 			choice = input.nextInt();
@@ -242,10 +245,10 @@ public class ScientificCalculator {
 		
 		switch(choice) {
 		case 1:
-			calculateTangent(25, factorials);
+			calculateTan(25, factorials);
 			menu();
 		case 2:
-			System.out.println("\nDetails of Γ(x)");
+			System.out.println("\nDetails of sinh(x)");
 			function3or4();
 			menu();
 		case 3:
