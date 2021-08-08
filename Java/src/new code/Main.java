d79030214916935e9d0f9c9a65e12cb9753b0c2c
Arnab Roy <royo.arnab@gmail.com>
2021-08-08 18:17:11 -0400

    Separated code into Math, Menu and Main classes

---
+++ Java/src/new code/Main.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void processTangent(){
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            Menu.promptTangentInput();
+            try {
+                String input = scanner.nextLine().trim();
+                if(input.length() == 1 && input.toLowerCase().equals("z")){
+                    break;
+                }
+                System.out.println("The output for tan(" + input +
+                        ") is " + Calculator.calculateTan(Integer.parseInt(input)));
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Your input needs to be an integer in the range -2147483648" +
+                        " to 2147483647. Please try again.");
+            } catch (ArithmeticException | NumberFormatException e) {
+                System.out.println("Invalid input! Please try again.");
+            }
+        }
+    }
+
+    public static void processABX(){
+        Menu.promptABXInput();
+        String a = Calculator.checkValid();
+        String b = Calculator.checkValid();
+        String c = Calculator.checkValid();
+
+        Calculator.function5or6(a, b, c);
+    }
+
+    public static void main(String[] args) {
+        int choice;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            Menu.printMenu();
+            Menu.promptMenuInput();
+            choice = 0;
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+                if (choice > 5 || choice < 1) {
+                    throw new InputMismatchException("Inappropriate input!");
+                }
+            } catch (InputMismatchException | NumberFormatException e) {
+                System.out.println("Input error!");
+            }
+
+            switch (choice) {
+                case 1:
+                    processTangent();
+                    break;
+                case 2:
+                    Menu.promptSineInput();
+                    Calculator.function3or4();
+                    break;
+                case 3:
+                    processABX();
+                    break;
+                case 4:
+                    Menu.promptXYInput();
+                    Calculator.function7or8();
+                    break;
+                case 5:
+                    Menu.printEnd();
+                    System.exit(0);
+            }
+        }
+
+    }
+}
---
+++ Java/src/new code/Menu.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Menu {
+    private static String printUnderLine(){
+        return "---------------------------------------------------------";
+    }
+
+    public static void printMenu(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(printUnderLine()).append("\n");
+        sb.append("Menu for supercalculator!\n");
+        sb.append(printUnderLine()).append("\n");
+        sb.append("1. tan(x)\n");
+        sb.append("2. sin(x)\n");
+        sb.append("3. ab^x\n");
+        sb.append("4. x^y\n");
+        sb.append(printUnderLine());
+        System.out.println(sb.toString());
+    }
+
+    public static void promptMenuInput(){
+        System.out.println("Please select an option from 1 to 5: ");
+    }
+
+    public static void promptTangentInput(){
+        System.out.println(printUnderLine());
+        System.out.println("You've chosen to calculate for tan(x).");
+        System.out.println("Enter an angle in degrees(needs to be integer) or press Z to return to main menu: ");
+    }
+
+    public static void promptSineInput(){
+        System.out.println(printUnderLine());
+        System.out.println("You've chosen to calculate for sin(x).");
+        System.out.println("Enter an angle in degrees(needs to be integer) or press Z to return to main menu:");
+    }
+
+    public static void promptABXInput(){
+        System.out.println(printUnderLine());
+        System.out.println("You've chosen to calculate for ab^x.");
+        System.out.println("Please enter a value for a, b and x in order and each value in a new line. Or press Z " +
+                "to return to main menu: ");
+    }
+
+    public static void promptXYInput(){
+        System.out.println(printUnderLine());
+        System.out.println("You've chosen to calculate for x^y.");
+        System.out.println("Please enter a value for x and y in order and each value in a new line. Or press Z to " +
+                "return to main menu: ");
+    }
+
+    public static void printEnd(){
+        System.out.println(printUnderLine());
+        System.out.println("Thank you! See you again!");
+        System.out.println(printUnderLine());
+    }
+}
